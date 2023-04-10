db51be78e7f639044c9047a45619a886a8f6ef1b
S. Andrei Dumitrescu <a.dumitrescu_s@yahoo.com>
2023-04-10 18:01:25 +0300

    Update MainGame.py

--- MainGame.py
+++ MainGame.py
@@ -1,4 +1,4 @@
-from Livetests import load_game, welcome # importing the functions load_game and welcome from module Livetests
+from Live import load_game, welcome # importing the functions load_game and welcome from module Livetests
 
 user_name = welcome() # calling the function welcome that will greet the user and get his name
                       # and we store it in a variable so the return value of welcome we use it in the load game function in this case will be the player name
