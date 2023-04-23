8ed78eaa7b5f09c42b1cfb3784c6b207077e5e08
S. Andrei Dumitrescu <a.dumitrescu_s@yahoo.com>
2023-04-23 16:58:41 +0300

    Update e2e.py

--- tests/e2e.py
+++ tests/e2e.py
@@ -11,7 +11,6 @@ def test_scores_service(app_url):
     driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
     driver.get(app_url)
 
-    # try:
     element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "score"))
     )
@@ -20,7 +19,7 @@ def test_scores_service(app_url):
         return True
     else:
         return False
-    # finally:
+
     driver.quit()
 
 def main_function():
